567093869987d9436506ae410a31596af29e4990
linhkhnguyen98 <107893768+linhkhnguyen98@users.noreply.github.com>
2024-06-18 16:19:09 -0700

    Update lab4.sv

--- lab4.sv
+++ lab4.sv
@@ -1,7 +1,3 @@
-// Lab 4
-// CSE140L
-// for use by registered cse140L students and staff only.
-// All rights reserved.
 module lab4 #(parameter DW=8, AW=8, byte_count=2**AW, lfsr_bitwidth=5)(
     output logic [7:0] encryptByte,   // encrypted byte
     output logic       validOut,      // encrypted byte is valid
